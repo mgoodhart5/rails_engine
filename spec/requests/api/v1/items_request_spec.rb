8e323385e8dd6ee44525367bcf4cfb68e66486fc
Mary Goodhart <mary.loves.pry@gmail.com>
2019-01-24 20:58:52 -0700

    beginning items requests

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe "Itemss API" do
-  xit "sends a list of items" do
+describe "Items API" do
+  it "sends a list of items" do
     create_list(:item, 3)
 
     get "/api/v1/items"
