15bb7f28403887227e9c254a4ae2a4c315cf6251
Mary Goodhart <mary.loves.pry@gmail.com>
2019-01-27 17:44:20 -0700

    spec harness testing

--- app/controllers/api/v1/transactions/search_controller.rb
+++ app/controllers/api/v1/transactions/search_controller.rb
@@ -11,6 +11,8 @@ class Api::V1::Transactions::SearchController < ApplicationController
       render json: TransactionSerializer.new(Transaction.where(updated_at: params[:updated_at]))
     elsif params[:invoice_id]
       render json: TransactionSerializer.new(Transaction.where(invoice_id: params[:invoice_id]))
+    elsif params[:credit_card_number]
+      render json: TransactionSerializer.new(Transaction.where(credit_card_number: params[:credit_card_number]))
     end
   end
 
@@ -20,11 +22,13 @@ class Api::V1::Transactions::SearchController < ApplicationController
     elsif params[:result]
       render json: TransactionSerializer.new(Transaction.where(result: params[:result]).first)
     elsif params[:created_at]
-      render json: TransactionSerializer.new(Transaction.where(created_at: params[:created_at]))
+      render json: TransactionSerializer.new(Transaction.where(created_at: params[:created_at]).first)
     elsif params[:updated_at]
-      render json: TransactionSerializer.new(Transaction.where(updated_at: params[:updated_at]))
+      render json: TransactionSerializer.new(Transaction.where(updated_at: params[:updated_at]).first)
     elsif params[:invoice_id]
-      render json: TransactionSerializer.new(Transaction.where(invoice_id: params[:invoice_id]))
+      render json: TransactionSerializer.new(Transaction.where(invoice_id: params[:invoice_id]).first)
+    elsif params[:credit_card_number]
+      render json: TransactionSerializer.new(Transaction.where(credit_card_number: params[:credit_card_number]).first)
     end
   end
 end
